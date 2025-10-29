57ca06439802d16e3a91c3666243715aae7c2e68
Simon Obonyo <simonobonyobarongo@gmail.com>
2025-10-29 17:21:20 +0300

    Add new SVG icons for alert, device, file, handMoney, searchStatus, success, and truckTick; update index.jsx to include new icons and export components for them

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -1,67 +1,214 @@
-import facebookIcon from '@/assets/icons/facebook.svg';
-import instagramIcon from '@/assets/icons/instagram.svg';
-import tiktokIcon from '@/assets/icons/tiktok.svg';
-import whatsappIcon from '@/assets/icons/whatsApp.svg';
-import xIcon from '@/assets/icons/x.svg';
-import linkIcon from '@/assets/icons/link.svg';
-import addIcon from '@/assets/icons/add.svg';
-import minusIcon from '@/assets/icons/minus.svg';
-import cartIcon from '@/assets/icons/cart.svg';
-import boxIcon from '@/assets/icons/box.svg';
-import truckIcon from '@/assets/icons/truck.svg';
-import verifyIcon from '@/assets/icons/verify.svg';
+import facebookIcon from '@/assets/icons/facebook.svg'
+import instagramIcon from '@/assets/icons/instagram.svg'
+import tiktokIcon from '@/assets/icons/tiktok.svg'
+import whatsappIcon from '@/assets/icons/whatsApp.svg'
+import xIcon from '@/assets/icons/x.svg'
+import linkIcon from '@/assets/icons/link.svg'
+import addIcon from '@/assets/icons/add.svg'
+import minusIcon from '@/assets/icons/minus.svg'
+import cartIcon from '@/assets/icons/cart.svg'
+import boxIcon from '@/assets/icons/box.svg'
+import truckIcon from '@/assets/icons/truck.svg'
+import verifyIcon from '@/assets/icons/verify.svg'
 import trashIcon from '@/assets/icons/trash.svg'
+import successIcon from '@/assets/icons/success.svg'
+import alertIcon from '@/assets/icons/alert.svg'
+import deviceIcon from '@/assets/icons/device.svg'
+import handMoneyIcon from '@/assets/icons/handMoney.svg'
+import fileIcon from '@/assets/icons/file.svg'
+import searchIcon from '@/assets/icons/searchStatus.svg'
+import truckTickIcon from '@/assets/icons/truckTick.svg'
 
+export const TruckTickIcon = ({ size, className = '' }) => (
+  <img
+    src={truckTickIcon}
+    alt="search"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
+
+export const SearchIcon = ({ size, className = '' }) => (
+  <img
+    src={searchIcon}
+    alt="search"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
+
+export const FileIcon = ({ size, className = '' }) => (
+  <img
+    src={fileIcon}
+    alt="file"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
+
+export const HandMoneyIcon = ({ size, className = '' }) => (
+  <img
+    src={handMoneyIcon}
+    alt="handMoney"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
+
+export const DeviceIcon = ({ size, className = '' }) => (
+  <img
+    src={deviceIcon}
+    alt="device"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
+
+export const AlertIcon = ({ size, className = '' }) => (
+  <img
+    src={alertIcon}
+    alt="alert"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
+
+export const SuccessIcon = ({ size, className = '' }) => (
+  <img
+    src={successIcon}
+    alt="success"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const TrashIcon = ({ size = 24, className = '' }) => (
-  <img src={trashIcon} alt="trash" width={size} height={size} className={className} />
-);
-
+  <img
+    src={trashIcon}
+    alt="trash"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const VerifyIcon = ({ size = 24, className = '' }) => (
-  <img src={verifyIcon} alt="Verify" width={size} height={size} className={className} />
-);
+  <img
+    src={verifyIcon}
+    alt="Verify"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const TruckIcon = ({ size = 24, className = '' }) => (
-  <img src={truckIcon} alt="Truck" width={size} height={size} className={className} />
-);
+  <img
+    src={truckIcon}
+    alt="Truck"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const BoxIcon = ({ size = 24, className = '' }) => (
-  <img src={boxIcon} alt="Box" width={size} height={size} className={className} />
-);
+  <img
+    src={boxIcon}
+    alt="Box"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const CartIcon = ({ size = 24, className = '' }) => (
-  <img src={cartIcon} alt="Cart" width={size} height={size} className={className} />
-);
+  <img
+    src={cartIcon}
+    alt="Cart"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const MinusIcon = ({ size = 24, className = '' }) => (
-  <img src={minusIcon} alt="Minus" width={size} height={size} className={className} />
-);
+  <img
+    src={minusIcon}
+    alt="Minus"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const AddIcon = ({ size = 32, className = '' }) => (
-  <img src={addIcon} alt="Add" width={size} height={size} className={className} />
-);
+  <img
+    src={addIcon}
+    alt="Add"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const LinkIcon = ({ size = 24, className = '' }) => (
-  <img src={linkIcon} alt="Link" width={size} height={size} className={className} />
-);
+  <img
+    src={linkIcon}
+    alt="Link"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const FacebookIcon = ({ size = 24, className = '' }) => (
-  <img src={facebookIcon} alt="Facebook" width={size} height={size} className={className} />
-);
+  <img
+    src={facebookIcon}
+    alt="Facebook"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const InstagramIcon = ({ size = 24, className = '' }) => (
-  <img src={instagramIcon} alt="Instagram" width={size} height={size} className={className} />
-);
+  <img
+    src={instagramIcon}
+    alt="Instagram"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const TikTokIcon = ({ size = 24, className = '' }) => (
-  <img src={tiktokIcon} alt="TikTok" width={size} height={size} className={className} />
-);
+  <img
+    src={tiktokIcon}
+    alt="TikTok"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const WhatsAppIcon = ({ size = 24, className = '' }) => (
-  <img src={whatsappIcon} alt="WhatsApp" width={size} height={size} className={className} />
-);
+  <img
+    src={whatsappIcon}
+    alt="WhatsApp"
+    width={size}
+    height={size}
+    className={className}
+  />
+)
 
 export const XIcon = ({ size = 24, className = '' }) => (
   <img src={xIcon} alt="X" width={size} height={size} className={className} />
-);
+)
