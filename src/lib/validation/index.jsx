28f1acb75ad796962e0330454dd17744fc216074
Eric <erickims08@gmail.com>
2025-11-10 09:57:07 +0300

    fix(validation): add missing comma in LoginSchema

--- src/lib/validation/index.jsx
+++ src/lib/validation/index.jsx
@@ -102,7 +102,7 @@ export const LoginSchema = zodResolver(
           return normalizeKenyanPhoneNumber(value)
         }
         return value.trim()
-      })
+      }),
 
     rememberMe: z.boolean().optional(),
   }),
